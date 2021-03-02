20c2c5bf101d72c9f31e660cf13586a4b11e9ac2
unknown <sabajaved524@gmail.com>
2021-03-02 10:42:26 +0500

    Latest Changes

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -5,11 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent implements OnInit {
+export class MoviesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  movies: any[] = [
+    [
+      {
+        "movieId": 1,
+        "movieName": "Batman",
+        "releaseDate": "March 20, 2012",
+        "movieType": "Episode",
+        "movieRuntime": "02:20:02",
+        "movieRating": 4.9,
+      },
+      {
+        "movieId": 1,
+        "movieName": "Harry Potter",
+        "releaseDate": "March 20, 2012",
+        "movieType": "Season",
+        "runTime": "02:20:02",
+        "rating": 4.9,
+      },
+      {
+        "movieId": 1,
+        "movieName": "Attack On Titan",
+        "releaseDate": "March 20, 2012",
+        "movieType": "Season",
+        "runTime": "02:20:02",
+        "rating": 4.9,
+      },
+    ]
+  ];
 
 }
